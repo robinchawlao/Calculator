bbc1f80e6bdae91b9562479bd79bac97412bcb2c
robinchawlao <125807916+robinchawlao@users.noreply.github.com>
2023-11-30 14:21:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./Components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
